17bc4171350231913cdc03006a4c64b324afb772
Mitch McFarland <mitch@pop-os.localdomain>
2020-05-08 21:55:09 -0700

    Add tests for dropPiece function

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -18,3 +18,84 @@ TEST(PiezasTest, sanityCheck)
 {
 	ASSERT_TRUE(true);
 }
+
+TEST(PiezasTest, check_dropPiece_X)
+{
+    Pieza game;
+    
+    game.dropPiece(0);
+
+    char piece = (char)game.pieceAt(0, 0);
+    
+    ASSERT_EQ(piece, 'X');
+}
+
+TEST(PiezasTest, check_dropPiece_O)
+{
+    Pieza game;
+    
+    game.dropPiece(0);
+    game.dropPiece(0);
+
+    char piece = (char)game.pieceAt(0, 0);
+    
+    ASSERT_EQ(piece, 'O');
+}
+
+TEST(PiezasTest, check_dropPiece_blank)
+{
+    Pieza game;
+    
+    game.dropPiece(0);
+    game.dropPiece(0);
+    game.dropPiece(0);
+
+    char piece = (char)game.pieceAt(0, 0);
+    
+    ASSERT_EQ(piece, ' ');
+}
+
+TEST(PiezasTest, check_pieceAt_and_dropPiece_valid)
+{
+    Pieza game;
+    
+    game.dropPiece(0);
+
+    char piece = (char)game.pieceAt(0, 0);
+    
+    ASSERT_EQ(piece, 'X');
+}
+
+TEST(PiezasTest, check_pieceAt_and_dropPiece_blank)
+{
+    Pieza game;
+    
+    game.dropPiece(0);
+
+    char piece = (char)game.pieceAt(1, 0);
+    
+    ASSERT_EQ(piece, ' ');
+}
+
+TEST(PiezasTest, check_pieceAt_and_dropPiece_invalid1)
+{
+    Pieza game;
+    
+    game.dropPiece(0);
+
+    char piece = (char)game.pieceAt(4, 0);
+    
+    ASSERT_EQ(piece, '?');
+}
+
+TEST(PiezasTest, check_pieceAt_and_dropPiece_invalid2)
+{
+    Pieza game;
+    
+    game.dropPiece(0);
+
+    char piece = (char)game.pieceAt(0, 5);
+    
+    ASSERT_EQ(piece, '?');
+}
+
